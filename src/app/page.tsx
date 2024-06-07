4af5d2cc2dbd5242f5b04aef524b3546aba84dbc
CodeMuscle <investportal99@gmail.com>
2024-06-07 11:01:09 +0530

    Work continued

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import { Metadata } from "next";
+import { Metadata } from 'next'
 
-import { SliceZone } from "@prismicio/react";
-import * as prismic from "@prismicio/client";
+import { SliceZone } from '@prismicio/react'
+import * as prismic from '@prismicio/client'
 
-import { createClient } from "@/prismicio";
-import { components } from "@/slices";
+import { createClient } from '@/prismicio'
+import { components } from '@/slices'
 
 // This component renders your homepage.
 //
@@ -13,23 +13,23 @@ import { components } from "@/slices";
 // Use the SliceZone to render the content of the page.
 
 export async function generateMetadata(): Promise<Metadata> {
-  const client = createClient();
-  const home = await client.getByUID("page", "home");
+  const client = createClient()
+  const home = await client.getByUID('page', 'home')
 
   return {
     title: prismic.asText(home.data.title),
     description: home.data.meta_description,
     openGraph: {
       title: home.data.meta_title ?? undefined,
-      images: [{ url: home.data.meta_image.url ?? "" }],
-    },
-  };
+      images: [{ url: home.data.meta_image.url ?? '' }]
+    }
+  }
 }
 
 export default async function Index() {
   // The client queries content from the Prismic API
-  const client = createClient();
-  const home = await client.getByUID("page", "home");
+  const client = createClient()
+  const home = await client.getByUID('page', 'home')
 
-  return <SliceZone slices={home.data.slices} components={components} />;
+  return <SliceZone slices={home.data.slices} components={components} />
 }
